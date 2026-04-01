f95cc1435562506cad246f205124cd60309b353c
Aa <aaroncymor@gmail.com>
2026-04-01 17:15:42 +0800

    Add: valid-tree - submission-20

---
+++ Data Structures & Algorithms/valid-tree/submission-20.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        adj = [[] for _ in range(n)]
+        for n1, n2 in edges:
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+        visited = set()
+        q = deque([(0, -1)])
+        visited.add(0)
+        while q:
+            node, par = q.popleft()
+            for nei in adj[node]:
+                if nei == par:
+                    continue
+                if nei in visited:
+                    return False
+                visited.add(nei)
+                q.append((nei, node))
+        return len(visited) == n
