a928f2205912dc8d1a3d29da95a06f2bd8ed90af
Aa <aaroncymor@gmail.com>
2026-09-09 12:39:47 +0800

    Add: valid-tree - submission-39

---
+++ Data Structures & Algorithms/valid-tree/submission-39.py
@@ -0,0 +1,35 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+
+        class DSU:
+
+            def __init__(self, n):
+                self.Parents = [i for i in range(n)]
+                self.Rank = [0] * n
+            
+            def find(self, u):
+                if u != self.Parents[u]:
+                    self.Parents[u] = self.find(self.Parents[u])
+                return self.Parents[u]
+            
+            def union(self, u, v):
+                parU, parV = self.find(u), self.find(v)
+                if parU == parV:
+                    return False
+                
+                if self.Rank[parU] < self.Rank[parV]:
+                    self.Parents[parU] = parV
+                    self.Rank[parV] += self.Rank[parU]
+                else:
+                    self.Parents[parV] = parU
+                    self.Rank[parU] += self.Rank[parV]
+                return True
+            
+        if len(edges) != n - 1:
+            return False
+        
+        dsu = DSU(n)
+        for u, v in edges:
+            if not dsu.union(u, v):
+                return False
+        return True
