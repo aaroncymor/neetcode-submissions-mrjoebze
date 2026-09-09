a283a6718e4aec9aea0d2afe115860228848e781
Aa <aaroncymor@gmail.com>
2026-09-09 12:07:18 +0800

    Add: course-schedule - submission-22

---
+++ Data Structures & Algorithms/course-schedule/submission-22.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = {n:[] for n in range(numCourses)}
+        for crs, pre in prerequisites:
+            adj[crs].append(pre)
+        
+        visited = set()
+        def dfs(crs):
+            if crs in visited:
+                return False
+            
+            if adj[crs] == []:
+                return True
+            
+            visited.add(crs)
+            for pre in adj[crs]:
+                if not dfs(pre):
+                    return False
+            visited.remove(crs)
+            adj[crs] = []
+            return True
+        
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+
+        return True
